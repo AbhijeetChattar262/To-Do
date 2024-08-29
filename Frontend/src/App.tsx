2ac862fe4df939e195e93e5950b40a4473034b2d
ChoudhariNikita <choudharinikita96@gmail.com>
2024-08-29 09:52:52 +0530

    App.tsx override

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,27 +1,24 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Todos from "./pages/Todos";
+import Privateroute from "./pages/Privateroutes";
 
 const App: React.FC = () => {
-  const isLoggedIn = !!localStorage.getItem('token');
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/todos"
-          element={isLoggedIn ? <Todos /> : <Navigate to="/login" replace />}
-        />
-        <Route
-          path="/"
-          element={isLoggedIn ? <Todos /> : <Login />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/todos" element={
+        <Privateroute>
+        <Todos />
+        </Privateroute>
+        } />
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
   );
 };
 
