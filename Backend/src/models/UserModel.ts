b368e347275b45ceec952e9dd942fe649852121e
siddhesh128 <siddheshj039@gmail.com>
2024-09-11 11:14:49 +0530

    Renamed userModel.ts to UserModel.ts and todoModel.ts to TodoModel.ts

--- Backend/src/models/UserModel.ts
+++ Backend/src/models/UserModel.ts
@@ -1,44 +1,25 @@
-import { DataTypes, Model, Optional } from "sequelize";
-import sequelize from "../db/connectDB";
-
-interface UserAttributes {
-    id: number;
-    username: string;
-    password: string;
-}
-
-interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
-
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    public id!: number;
-    public username!: string;
-    public password!: string;
-
-   
-}
-
-User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        username: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-        },
-    },
-    {
-        tableName: "users",
-        sequelize,
-        timestamps: false, 
+export class UserDbModel{
+    public static getModel(sequelize : any, DataTypes : any) {
+        return sequelize.define("users", {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            username: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            tableName: "users",
+            timestamps: false,
+        });
     }
-);
-
-export default User;
+}
+        
